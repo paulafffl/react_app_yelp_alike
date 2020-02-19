b6463ce9ff605872d6dd1e7bab2608c46e560df3
Paula <paulafffl@gmail.com>
2020-02-19 08:27:10 +0000

    Added methods on return statement

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -58,8 +58,8 @@ class SearchBar extends React.Component {
           </ul>
         </div>
         <div className="SearchBar-fields">
-          <input placeholder="Search Businesses" />
-          <input placeholder="Where?" />
+          <input placeholder="Search Businesses" onChange={this.handleTermChange}/>
+          <input placeholder="Where?" onChange={this.handleLocationChange}/>
         </div>
         <div className="SearchBar-submit">
           <a href="www.#.com">Let's Go</a>
