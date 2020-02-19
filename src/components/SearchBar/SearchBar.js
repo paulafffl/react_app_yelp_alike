ff8e46fa4dec9f0acd43999399b13b3d8af72190
Paula <paulafffl@gmail.com>
2020-02-19 08:51:35 +0000

    Added Handle a Search

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -16,6 +16,7 @@ class SearchBar extends React.Component {
     };  
     this.handleTermChange = this.handleTermChange.bind(this);
     this.handleLocationChange = this.handleLocationChange.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
   }
 
   getSortByClass(sortByOption){
@@ -36,6 +37,11 @@ class SearchBar extends React.Component {
 
   handleLocationChange(event){
     this.setState({location: event.target.value});
+  }
+
+  handleSearch(event){
+    this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy)
+    event.preventDefault()
   }
 
   renderSortByOptions() {
@@ -62,7 +68,7 @@ class SearchBar extends React.Component {
           <input placeholder="Where?" onChange={this.handleLocationChange}/>
         </div>
         <div className="SearchBar-submit">
-          <a href="www.#.com">Let's Go</a>
+          <a href="www.#.com" onClick={this.handleSearch}>Let's Go</a>
         </div>
       </div>
     );
