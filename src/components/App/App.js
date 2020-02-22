90d2b597f5b0c4316280d6aa14c4657a787eeb8a
Paula <paulafffl@gmail.com>
2020-02-22 14:59:47 +0000

    Imported Yelp, removed hard coded bits and added a constructor to the searchYelp method

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,29 +3,17 @@ import './App.css';
 
 import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
-
-const business = {
-  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-  name: 'MarginOtto Pizzeria',
-  address: '1010 Paddington Way',
-  city: 'Bordertown',
-  state: 'NY',
-  zipCode: '10101',
-  category: 'Italian',
-  rating: 4.5,
-  reviewCount: 90
-};
-
-const businesses = [
-  business,
-  business,
-  business,
-  business,
-  business,
-  business
-];
+import Yelp from '../../util/Yelp'
 
 class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      businesses = []
+    };
+    this.searchYelp = this.searchYelp.bind(this);
+  }
+
   searchYelp(term, location, sortBy) {
     console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`);
   }
