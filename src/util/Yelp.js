6fdf7a1f19c7432f7a483711a4777d5a7ad40b8f
Paula <paulafffl@gmail.com>
2020-02-22 12:57:08 +0000

    Created search method to retrieve search results from Yelp API

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,1 +1,24 @@
 const apiKey = '5Om_vHjbTmCuN3inxw_7Xgk1kbovx3iu_gvP-zgubozTpc8U4AuaGN0TfmClrGSbH3A26KT_mZYdapZZLHLRrPD5CfBUEHDfEif1BG1W7lKDuoI_F0vmmJGSXPlPXnYx'
+
+const yelp = {
+    search(term, location, sortBy){
+        return fetch(
+            `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&$location=${LOCATION}&sort_by=${SORT_BY}`,{
+                headers: {
+                    Authorisation: `Bearer ${apiKey}`
+            }
+        //convert the returned response to JSON for us to be able to effectively utilize our list of businesses
+        }).then(response => {
+            return response.json();
+        //retrieve the list of businesses from the converted JSON response    
+        }).then(jsonResponse) => {
+            if (jsonResponse.businesses) {
+                return jsonResponse.businesses.map(business => {
+                    
+                })
+
+            }
+        }
+        ;
+    }
+}
